656399938791f6b77c2a1148d8e11640262f31f3
SkywalkerDarren <yh83533010yh2@gmail.com>
2021-05-30 01:20:39 +0800

    migrate to null safety

--- flutter_bloc_arch/lib/presentation/pages/home_page.dart
+++ flutter_bloc_arch/lib/presentation/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
+  HomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
